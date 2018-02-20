1774fd9dd6b2ce464fe9fcb0495c7e5bcec13a9d
Kononenko Anastasya <lily.willy@mail.ru>
2018-02-20 19:30:29 +0700

    stdout desk

---
+++ desk.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("  ---------------\n"
+           "8|r n b q k b n r|\n"
+           "7|p p p p p p p p|\n"
+           "6|               |\n"
+           "5|               |\n"
+           "4|               |\n"
+           "3|               |\n"
+           "2|P P P P P P P P|\n"
+           "1|R N B Q K B N R|\n"
+           "  ---------------\n"
+           "  a b c d e f g h \n\n"
+                                 );
+    return 0;
+}
